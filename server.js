5ca8ea40d6e7dfa9425b412c25d466d3b9684b7c
cokbie <jit.shen@hotmail.com>
2021-01-21 14:10:45 +0800

    update server.js

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 //Install express server    
 const express = require('express');
 
-const path = require('path');   
+
 
 const app = express();   
 
 // Serve only the static files form the dist directory    
-app.use(express.static('./dist/dota-test'));
+app.use(express.static('src'));
 
-app.get('/*', function(req,res) {  
-    res.sendFile(path.join('./dist/dota-test/index.html'));   
+app.listen(8080, function(){
+  console.log("Listening on port 8080!")
+});
+
+app.get('/', function(req,res) {  
+    res.sendFile('index.html');   
 });  
 
 // Start the app by listening on the default Heroku port    
-app.listen(process.env.PORT || 8080);
+//app.listen(process.env.PORT || 8080);
